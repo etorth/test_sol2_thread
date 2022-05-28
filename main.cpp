4354042863777646beb260ca45f160ad8b5c6b1e
etorth <anhonghe@gmail.com>
2022-05-27 22:26:01 -0700

    f

--- main.cpp
+++ main.cpp
@@ -32,34 +32,109 @@ int main()
     sol::state lua;
     lua.open_libraries();
 
-    lua.script_file("luamodule_tlscfg.lua");
-    sol::environment sandbox_env(lua, sol::create);
-    sandbox_env[sol::metatable_key] = lua.create_table();
-    sol::table sandbox_env_metatable = sandbox_env[sol::metatable_key];
+    lua.script(R"(
+        local _G = _G
+        local error = error
+        local coroutine = coroutine
 
-    sandbox_env_metatable["__index"] = sol::function(lua["sandBox_meta_index"]);
-    sandbox_env_metatable["__newindex"] = sol::function(lua["sandBox_meta_newindex"]);
+        local _G_sandbox = {}
+        function clearTLSTable()
+            local threadId, inMainThread = coroutine.running()
+            if inMainThread then
+                error('call clearTLSTable in main thread')
+            else
+                _G_sandbox[threadId] = nil
+            end
+        end
+
+        replaceEnv_metatable = {
+            __index = function(table, key)
+                local threadId, inMainThread = coroutine.running()
+                if not inMainThread then
+                    if _G_sandbox[threadId] ~= nil and _G_sandbox[threadId][key] ~= nil then
+                        return _G_sandbox[threadId][key]
+                    end
+                end
+                return _G[key]
+            end,
+
+            __newindex = function(table, key, value)
+                local threadId, inMainThread = coroutine.running()
+                if inMainThread then
+                    _G[key] = value
+                else
+                    if _G_sandbox[threadId] == nil then
+                        _G_sandbox[threadId] = {}
+                    end
+                    _G_sandbox[threadId][key] = value
+                end
+            end
+        }
+    )");
+
+    sol::environment replaceEnv(lua, sol::create);
+    replaceEnv[sol::metatable_key] = sol::table(lua["replaceEnv_metatable"]);
 
     // idea from: https://blog.rubenwardy.com/2020/07/26/sol3-script-sandbox/
-    // important hack to set sandbox_env as default environment
-    //
-    // otherwise I didn't know how to make LuaThreadRunner call with sandbox env
-    // if we let npc_main call back to C, I get error: attempt to yield from outside a coroutine
+    // set replaceEnv as default environment, otherwise I don't know how to setup replaceEnv to thread/coroutine
 
-    lua_rawgeti(lua.lua_state(), LUA_REGISTRYINDEX, sandbox_env.registry_index());
+    lua_rawgeti(lua.lua_state(), LUA_REGISTRYINDEX, replaceEnv.registry_index());
     lua_rawseti(lua.lua_state(), LUA_REGISTRYINDEX, LUA_RIDX_GLOBALS);
 
-    lua.script_file("npc.lua");
+    lua.script(R"(
+        function coth_main(runner)
+            local threadId, mainThread = coroutine.running()
+            if mainThread then
+                error('coth_main(runner) called in main thread', runner)
+            end
 
-    LuaThreadRunner runner1(lua, "npc_main");
+            -- test require
+            -- require should still work and accesses global variable: package
+
+            local mod = require('mod')
+            print(mod)
+
+            coroutine.yield()
+
+            counter = 0     -- localized global varible tested
+            counterMax = 10 --
+
+            while counter < counterMax do
+                print(string.format('runner %d counter %d, you can resume %d more times', runner, counter, counterMax - counter - 1))
+                counter = counter + 1
+                coroutine.yield()
+            end
+
+            clearTLSTable()
+        end
+    )");
+
+    LuaThreadRunner runner1(lua, "coth_main");
     checkError(runner1.callback(1));
 
-    LuaThreadRunner runner2(lua, "npc_main");
+    LuaThreadRunner runner2(lua, "coth_main");
     checkError(runner2.callback(2));
 
+    const auto fnResume = [](auto &callback, int index)
+    {
+        if(callback){
+            checkError(callback());
+        }
+        else{
+            std::cout << "runner " << index << " has exited" << std::endl;
+        }
+    };
+
     while(runner1.callback || runner2.callback){
-        if(runner1.callback) checkError(runner1.callback());
-        if(runner2.callback) checkError(runner2.callback());
+        int event_from = 0;
+        std::cout << "wait event from: ";
+        std::cin >> event_from;
+
+        switch(event_from){
+            case 1: fnResume(runner1.callback, 1); break;
+            case 2: fnResume(runner2.callback, 2); break;
+            default: std::cout << "no runner " << event_from << std::endl;
+        }
     }
     return 0;
 }
